4662faed5b231157f0eb07c77c36e733defa6a5a
kavetech2023 <kavetech@gmail.com>
2024-05-21 10:30:40 +0300

    add angle brackets

--- 0x01-react_intro/task_1/dashboard/src/App.js
+++ 0x01-react_intro/task_1/dashboard/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 function App() {
   return (
+    <>
     <div className="App">
       <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -17,6 +18,7 @@ function App() {
         <p> {getFullYear()} - {getFooterCopy()}</p>
       </div>
     </div>
+    </>
   );
 }
 
